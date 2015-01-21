4f4ac116091443831af0e3c7615ece942733f028
Qiuyu Jiang <jiangqy8@gmial.com>
2015-01-21 06:24:08 -0500

    simple HW server

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+var http=require("http");
+var port=process.env.PORT||1337;
+var server=http.createServer(function(req,res){
+	res.writeHead(200,{"Content-Type":"text/plain"});
+	res.end("Hello world");
+});
+server.listen(port,function(){
+	console.log("Server is running at %s:%s",server.address().address,server.address().port);
+});
